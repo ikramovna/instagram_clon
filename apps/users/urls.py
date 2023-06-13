a2a5676cdd7df284ef307b8a7339c85962d4abc3
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-14 00:29:58 +0500

    update

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+
+from apps.users.views import UserCreateAPIView
+
+urlpatterns = [
+    path('signup', UserCreateAPIView.as_view())
+
+]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from apps.users.models import User
+from apps.users.serializers import SignUpSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = SignUpSerializer
