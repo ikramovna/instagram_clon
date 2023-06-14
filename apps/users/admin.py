42dbe92a6ebcae0303ee9982a07b87878915c1f8
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-14 23:13:01 +0500

    update

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'phone_number']
+
+
+admin.site.register(User)
--- root/settings.py
+++ root/settings.py
@@ -110,3 +110,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = True
