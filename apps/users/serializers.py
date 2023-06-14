13141ff6ef2918d181cd9a2e5722fe4dc7c66ef1
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-14 22:40:16 +0500

    update

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.fields import CharField
 from rest_framework.serializers import ModelSerializer, UUIDField
 
-from apps.users.models import User
+from apps.shared.utility import check_email_or_phone
+from apps.users.models import User, VIA_EMAIL, VIA_PHONE
 
 
 class SignUpSerializer(ModelSerializer):
@@ -11,8 +13,6 @@ class SignUpSerializer(ModelSerializer):
     def __int__(self, *args, **kwargs):
         super(SignUpSerializer, self).__init__(*args, **kwargs)
         self.fields['email_phone_number'] = CharField(required=False)
-
-
 
     class Meta:
         model = User
@@ -33,7 +33,24 @@ class SignUpSerializer(ModelSerializer):
         return data
 
     @staticmethod
-    def auth_validate(attrs):
-        print(attrs)
-        user_input = str(attrs.get('email_phone_number')).lower()
+    def auth_validate(data):
+        print(data)
+        user_input = str(data.get('email_phone_number')).lower()
+        input_type = check_email_or_phone(user_input)
+        if input_type == "email":
+            data = {
+                'email': user_input,
+                'auth_type': VIA_EMAIL
+            }
+        elif input_type == 'phone':
+            data = {
+                'phone_number': user_input,
+                'auth_type': VIA_PHONE
+            }
+        else:
+            data = {
+                'success': False,
+                'message': 'You must send email or phone number'
+            }
+            raise ValidationError(data)
         return data
